54ac361b6cf398b4e870b8d079e3fb25017a54f6
Manuel Izquierdo <mizq14@gmail.com>
2026-04-15 23:37:54 +1000

    Update xero.py

--- integrations/xero.py
+++ integrations/xero.py
@@ -7,7 +7,7 @@ from urllib.parse import urlencode
 XERO_AUTH_URL  = "https://login.xero.com/identity/connect/authorize"
 XERO_TOKEN_URL = "https://identity.xero.com/connect/token"
 XERO_API_BASE  = "https://api.xero.com/api.xro/2.0"
-SCOPES         = "openid profile email accounting.invoices accounting.contacts offline_access"
+SCOPES         = "openid profile email accounting.invoices accounting.contacts accounting.settings offline_access"
 
 BRANDING_THEME_NAME = "MAD IT WORKS RESELLER"
 
